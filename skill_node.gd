94756c7cfa57e27381c15bdac97863bd79e55e97
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2026-08-26 22:25:20 +0200

    added exportable stats; added simple player

--- skill_node.gd
+++ skill_node.gd
@@ -5,19 +5,36 @@ class_name SkillNode
 @onready var panel = $Panel
 @onready var node_label = $description_container/description
 @onready var connection_line = $connection_line
+@onready var skill_node: SkillNode = $"."
 
-var level : int = 0:
-	set(value):
-		level = value
-		node_label.text = "soslo / " + str(level)
+@onready var player = get_tree().get_first_node_in_group("player")
+
+@export var level: int:
+	set = set_level
+@export var node_name: String
+@export var icon: Image
+@export var description_stats: Array[String]
+
+
+func set_level(value):
+	level = value
+	node_label.text = "soslo / " + str(level)
 
 func _ready():
+	if description_stats.size() > 0:
+		skill_node.tooltip_text = description_stats[0]
+	
 	if get_parent() is SkillNode:
 		connection_line.add_point(global_position + size/2)
 		connection_line.add_point(get_parent().global_position + size/2)
 
 func _on_pressed() -> void:
+	var old_level = level
 	level = min(level+1, 5)
+	
+	if old_level != level:
+		player.level += 1
+	
 	panel.show_behind_parent = true
 	
 	connection_line.default_color = Color(0.804, 0.027, 1.0, 1.0)
